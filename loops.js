a71a4909d80be45fdf7e4be708afa6e20252e7d6
saraperse <srh.rouhani@gmail.com>
2021-04-07 17:12:39 +0200

    loops-js

---
+++ loops.js
@@ -0,0 +1,53 @@
+/*var number = 0;
+while (number < 3) {
+    console.log(number);
+    number++;
+}
+
+
+
+
+var number = 0;
+do {
+    console.log(number);
+    number++;
+}
+while (number < 3)*/
+
+
+
+
+
+var number = 0;
+while (number > 0 && number < 3); {
+    console.log(number);
+    number++;
+
+}
+
+
+var number = 0;
+do {
+    console.log(number);
+    number++;
+}
+while (number > 0 && number < 3)
+
+
+//syntaxe de for- ça permet de mettre souvent à jour les choses
+
+
+/*for (partie1; partie2; partie3)
+{
+    code A executer
+}*/
+    
+
+
+
+
+for (var number = 0; number < 5; number++) 
+{
+    console.log(number);
+
+}

fe6cc6f3de93884740d8c16c8c75aa966dc00d82
saraperse <srh.rouhani@gmail.com>
2021-04-07 10:07:25 +0200

    boolean&comparaison

--- script.js
+++ script.js
@@ -1,8 +1,176 @@
-/*var premiervariable = 'montexte';
+/*variable numbers § strings*/
+
+
+/*var x = "name";
+var Y = "name.length";
+console.log(name.length);*/
+var kaka = "Montexte";
+console.log(kaka);
+
+var x = 13;
+console.log(x);
+
+var x = 12;
+var y = 1;
+var z = x + y;
+console.log(x + y);
+
+
+var x = 12.56;
+var quo = x.toString;
+console.log(x);
+
+var name = "Marabou";
+console.log(name);
+
+
+var name = 'jetedeteste'
+var unname = name.length;
+console.log(unname);
+
+
+
+
+var premiervariable = 'montexte';
 
 var deuxièmevariable = premiervariable.length;
 
-console.log(premiervariable.length);*/
-var x = "name";
-var Y = "name.length";
-console.log(name.length);
+console.log(deuxièmevariable);
+
+
+var code = "12";
+console.log(code);
+
+var code = 'hello   sarah';
+var incode = code.indexOf("sarah");
+console.log(incode);
+
+var code = 'hello sarah';
+var incode = code.replace("sarah", "Marzy");
+console.log(incode);
+
+var string1 = "hello";
+var string2 = "Badboy";
+var myNewString = string1 + string2;
+console.log(myNewString);
+
+/*variable Operators . Ils ne fonctionnent pas pareilles sur les nombres et les strings*/
+
+var x = 11;
+var y = 5;
+x--;
+console.log(x);
+
+
+var x = 11;
+var y = 5;
+x++;
+console.log(x);
+
+
+var x = 17;
+x++;
+console.log(x);
+
+// booleans & comparisons
+
+
+
+
+var x = 13;
+var y = 5;
+x = x + y; /* x+=y;*/
+console.log(x);
+
+var string1 = "hello";
+var string2 = "la vie";
+
+
+var myBoolean = true;
+console.log(myBoolean);
+
+
+
+
+//opérateurs de comparaison
+
+
+var x = 7;
+var myBoll = (x===5);
+console.log(myBoll);
+
+var x = 8;
+var myBoolean = (x!==0);
+console.log(myBoolean);
+
+//comparaison égale et strictement égale ( numbers et strings)
+
+var x = 7;
+var myBoll = (x==="7");
+console.log(myBoll);
+
+var x = 7;
+var myBoll = (x===7);
+console.log(myBoll);
+
+var x = 7;
+var myBoll = (x>4);
+console.log(myBoll);
+
+var x = 7;
+var myBoll = x<1;
+console.log(myBoll);
+
+//lier plusieurs questions dans une même question
+
+var x = 5;
+var y = 12;
+var boolean = x>3 && y>15;
+console.log(boolean);
+
+var x = 5;
+var y = 12;
+var boolean = x>3 && y<15;
+console.log(boolean);
+
+var x = 5;
+var y = 12;
+var boolean = x>3 || y>15;
+console.log(boolean);
+
+var x = 5;
+var y = 12;
+var boolean = x>3 || y>15;
+console.log(boolean);
+
+// && et ||
+
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+console.log(true || true);
+console.log(true || false);
+console.log(false|| true);
+console.log(false|| false);
+
+
+console.log('-----------------');
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+console.log('---------------------');
+console.log(true || true);
+console.log(true || false);
+console.log(false|| true);
+console.log(false|| false);
+console.log("-----------------------");
+console.log(!true);
+console.log(!false);
+
+
+
+
+
